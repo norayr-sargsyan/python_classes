5fc96ad62490319f152ce98bd2746151775553f4
Norayr <sargsyanns001@gmail.com>
2022-11-16 13:50:19 +0400

    homework

--- 111222jpeg.py
+++ 111222jpeg.py
@@ -3,24 +3,22 @@ import json
 
 
 class Pictures:
+    image_list = []
 
     def __init__(self, json_with_pics):
         self.pics = json_with_pics
-        self.image_list = []
 
     def image_in_json(self):
-        with open(self.pics, "r") as pics_:
-            fine = json.load(pics_)
-
-            for item in fine:
-                for i in item:
-                    if i.endswith(".jpg") or i.endswith(".jpeg") or i.endswith(".png"):
-                        self.image_list.append(i)
-            return self.image_list
+        with open(self.pics, "r") as pictures:
+            fine = json.load(pictures)
+        for i in fine:
+            if fine[i].endswith("jpg") or fine[i].endswith("jpeg") or fine[i].endswith("png"):
+                self.image_list.append(fine[i])
+        return self.image_list
 
     def jpeg_download(self, name):
         for url in self.image_list:
-            if url.endswith(".jpeg"):
+            if url.endswith("jpeg"):
                 try:
                     response = requests.get(url)
                 except Exception as err:
@@ -31,10 +29,11 @@ class Pictures:
                 if s_code.startswith("2"):
                     with open(f"{name}.jpeg", "wb") as pics:
                         pics.write(response.content)
+        return f"Done: {name} is downloaded"
 
     def png_download(self, name):
         for url in self.image_list:
-            if url.endswith(".png"):
+            if url.endswith("png"):
                 try:
                     response = requests.get(url)
                 except Exception as err:
@@ -46,6 +45,8 @@ class Pictures:
                     with open(f"{name}.png", "wb") as pics:
                         pics.write(response.content)
 
+        return f"Done: {name} is downloaded"
 
-picture = Pictures(json_with_pics="sample_json.json")
+
+picture = Pictures("sample_json.json")
 print(picture.image_in_json(), picture.png_download(name="new_png"), picture.jpeg_download(name="new_jpeg"))
