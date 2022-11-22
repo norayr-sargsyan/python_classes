8ba8b0370a9d6c9e21dae4ebaa35124e81156bbf
Norayr <sargsyanns001@gmail.com>
2022-11-22 18:28:58 +0400

    homework

--- 111822thread.py
+++ 111822thread.py
@@ -1,29 +1,37 @@
 import threading
 import json
 import requests
+from multiprocessing import Pool
 
 
-image_list = []
-with open("sample_json.json", "r") as json_pics:
-    pics = json.load(json_pics)
-    for i in pics:
-        image_list.append(pics[i])
+class Downloader:
+    def __init__(self, json_file):
+        self.image_list = []
+        with open(json_file) as json_pics:
+            pics = json.load(json_pics)
+            for i in pics:
+                self.image_list.append(pics[i])
 
+    def img_downloading(self, url, name):
+        print("started", name)
+        try:
+            response_ = requests.get(url)
+        except requests.exceptions.ConnectionError as err:
+            print(f"smth happened {err}")
+            return
 
-def downloading(dict_):
-    url = dict_["image-url"]
-    name = dict_["name"]
-    try:
-        response = requests.get(url)
-    except Exception as err:
-        print(f"{err} ")
-        return
-    s_code = str(response.status_code)
-    if s_code.startswith("2"):
-        with open(f"{name}.jpeg", "wb") as picture:
-            picture.write(response.content)
-    print(f"{name} image is downloaded")
+        s_code = str(response_.status_code)
+        if s_code.startswith("2"):
+            with open(f"{name}", "wb") as photo:
+                photo.write(response_.content)
+                print(f"image {name} is downloaded")
 
-for i in range(len(image_list)):
-    x = threading.Thread(target=downloading, args=(image_list[i],))
-    x.start()
+    def td_download(self):
+        new_list = []
+        for item in self.image_list:
+            x = threading.Thread(target=self.img_downloading, args=(item["name"], item["image-url"]))
+            new_list.append(x)
+            x.start()
+        for img in new_list:
+            img.join()
+        print("Done: all images is downloaded")
